00417441ff957f9f7c1f6ddac55cdf8eb1b03d01
Armands Dunskis <armands.dunskis2@mail.dcu.ie>
2023-06-20 19:44:20 +0100

    Fixed function not found due to onclick scope & added remove function

--- LinkedList/LinkedList.ts
+++ LinkedList/LinkedList.ts
@@ -1,83 +1,107 @@
 class LLNode {
-  item: string;
-  next: LLNode | null;
+  item: string
+  next: LLNode | null
 
   constructor(item: string){
-    this.item = item;
-    this.next = null;
+    this.item = item
+    this.next = null
   }
 
   getLastNode(): LLNode {
-    if(this.next == null){return this;}
-    return this.next.getLastNode();
+    if(this.next == null){return this}
+    return this.next.getLastNode()
   }
 }
 
 class LinkedList {
-  head: LLNode | null;
+  head: LLNode | null
   
   constructor(){
-    this.head = null;
+    this.head = null
   }
 
   insertFront(item: string){
     if(this.head == null){
-      this.head = new LLNode(item);
-      return;
+      this.head = new LLNode(item)
+      return
     }
     let node: LLNode = new LLNode(item)
-    node.next = this.head;
-    this.head = node;
+    node.next = this.head
+    this.head = node
   }
 
   insertBack(item: string){
-    if(this.head == null){this.head = new LLNode(item);}
-    let lastNode: LLNode | null = this.head.getLastNode();
-    lastNode.next = new LLNode(item);
+    if(this.head == null){this.head = new LLNode(item)}
+    let lastNode: LLNode | null = this.head.getLastNode()
+    lastNode.next = new LLNode(item)
   }
 
   getLastNode(): LLNode | null{
-    if(this.head == null){return null;}
-    return this.head.getLastNode();
+    if(this.head == null){return null}
+    return this.head.getLastNode()
   }
 }
 
 class LLDomController {
-  linked_list : LinkedList;
+  linked_list : LinkedList
+  count: number = 0
 
   constructor(){
-    this.linked_list = new LinkedList();
+    this.linked_list = new LinkedList()
   }
 
-  createLLElement(item: string){
-    const el = document.createElement('div');
-    el.setAttribute('id', 'll_element');
-    const p = document.createElement('p');
-    p.innerHTML = item;
-    el.append(p);
-    return el;
+  createLLElement() {
+    const ll_element = document.createElement('div')
+    ll_element.setAttribute('id', 'll_element')
+    const p = document.createElement('p')
+    p.innerHTML = 'item' + this.count
+    this.count += 1
+    ll_element.append(p)
+    return ll_element
   }
 
-  ex(){
-    console.console.log('1');
-    
-  }
-  
-  add(){
-    const ll_holder = document.getElementById('ll_holder');
+  add(): undefined {
+    const ll_holder = document.getElementById('ll_holder')
     if(ll_holder ==  null){
-      console.log('Err: ll_holder is null');
-      return;
+      console.log('Err: ll_holder is null')
+      return
     }
-    this.createLLElement('item')
-    ll_holder.append(this.createLLElement('item'));
+    const ll_element = this.createLLElement()
+    ll_holder.append(ll_element)
   }
+
+  remove(): undefined {
+    const ll_holder = document.getElementById('ll_holder')
+    if(! ll_holder){
+      console.log('Err: ll_holder is null')
+      return
+
+    }
+
+    if(! ll_holder.hasChildNodes()){
+      console.log('War: ll_holder has no children')
+      return
+    }
+
+    const ll_element_count = ll_holder.children.length
+    ll_holder.removeChild(ll_holder.children[ll_element_count - 1])
+
+  }
+
 }
 
 
 function main(){
-  let LL: LLDomController = new LLDomController();
-  document.getElementById('add_element').addEventListener('click', LL.add);
+  let LL: LLDomController = new LLDomController()
+  let add_btn = document.getElementById('add_element')
+  if(add_btn){
+    add_btn.onclick = () => LL.add()
+  }
+  let remove_btn = document.getElementById('del_element')
+  if(remove_btn){
+    remove_btn.onclick = () => LL.remove()
+  }
 }
 
 main()
+
